4f034b4be94f97ae3f34a36a4b4393c011dc9c1b
walkerkd <kristiwalker22@gmail.com>
2016-03-28 22:09:14 -0400

    Updating light and adding images

--- light.js
+++ light.js
@@ -1,15 +1,39 @@
-var light = new createjs.Bitmap("assets/cone.jpg");
+var light = new createjs.Bitmap("assets/cone.png");
     light.crossOrigin="Anonymous";
-    light.x = 270;
-    light.y = 190;
+    light.x = 190;
+    light.y = 0;
     light.width = (h/5);
     light.height = (h/5);
     light.setBounds(light.x, light.y, light.width, light.height);
     //setbounds x, y, w, h
-    coralBounds = light.getBounds();
-    //coralHeight = coral.height;
+    lightBounds = light.getBounds();
+    //lightHeight = light.height;
     console.log("light bounds is " + lightBounds);
 
 light.image.onload = function() {
   stage.update();
 };
+
+// function brightenLight() {
+//     console.log("We're at the brighten light function");
+//     //when newFood is an image, it wont let you create multiple ones; need to store in array and rename or something
+//     var brightLight = new createjs.Bitmap("assets/empty-cone.png"); //add to array; when deleted, POP it
+//     //foodArr.push(newFood);
+//     brightLight.x = 290;
+//     brightLight.y = 0;
+//     //newFood = new createjs.Shape();
+//     //newFood.graphics.beginFill("blue").drawCircle(0, 0, 20);
+//     stage.update();
+//     tankContainer.addChild(brightLight);
+//     stage.update();
+// }
+//
+//
+// // when the mouse is clicked, make a new light instance and make this draggable
+// lightControls.addEventListener("mousedown", function (evt) {
+//   console.log("Hi");
+// if (evt.nativeEvent.button >= 0) {
+//     brightenLight();
+// }
+// stage.update();
+// });
