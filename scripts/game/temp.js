1ed1788e17aed8bd9725b6fd81e9bb72a97a3a7a
Andy Roberson <andyroberson2@gmail.com>
2016-04-17 23:10:29 -0400

    changing layout of temperature button, adding correct graphic, changing style to match temp. button graphic

--- scripts/game/temp.js
+++ scripts/game/temp.js
@@ -4,8 +4,8 @@ function createTemp() {
   var tempContainer = new createjs.Container();
       tempContainer.x = (rightControlMenu.x + 20); //10 padding
       tempContainer.y = (leftMenuHeight/6);
-      tempContainer.width = (leftControlMenu.width - 20); //-20 for 10 padding on left and right
-      tempContainer.height = leftMenuHeight/4;
+      tempContainer.width = (leftControlMenu.width * (70/113)); //-20 for 10 padding on left and right
+      tempContainer.height = 102;
       tempHeight = tempContainer.height;
       tempWidth = tempContainer.width;
       tempContainer.setBounds(tempContainer.x, tempContainer.y, tempContainer.width, tempContainer.height);
@@ -14,30 +14,28 @@ function createTemp() {
 
 
   //bg for temperature graphics
-  var tempBackground = new createjs.Shape();
-      tempBackground.graphics.beginFill("Teal").drawRect(0, 0, tempWidth, tempHeight);
+  var tempBackground = new createjs.Bitmap("assets/temp-bg2.png");
+      tempBackground.x = 0;
+      tempBackground.y = 0;
+
 
   //container for screen that shows temp
   var screenContainer = new createjs.Container();
-      screenContainer.x = (tempWidth * (1/7));
-      screenContainer.y = (tempHeight * (1/4));
-      screenContainer.width = (tempWidth * (1/2));
-      screenContainer.height = (tempHeight * (2/4));
+      screenContainer.x = (tempWidth * (1/8));
+      screenContainer.y = (tempHeight * (2/7));
+      screenContainer.width = (tempWidth * (6/8));
+      screenContainer.height = (tempHeight * (3/7));
 
       screenHeight = screenContainer.height;
       screenWidth = screenContainer.width;
       screenContainer.setBounds(screenContainer.x, screenContainer.y, screenContainer.width, screenContainer.height);
-
-  //background for temperature window
-  var tempScreen = new createjs.Shape();
-  tempBackground.graphics.beginFill("#EEE").drawRect(screenContainer.x, screenContainer.y, screenWidth, screenHeight);
 
   var degrees = 70.0;
 
   //http://www.createjs.com/docs/easeljs/classes/Graphics.html#method_drawPolyStar
   //drawPolyStar(x, y, radius, sides, pointsize, angle)
   var upArrow = new createjs.Shape();
-  upArrow.graphics.beginFill("#eee").drawPolyStar((tempWidth*(4/5)), (tempHeight*(1/3)), (tempWidth*(1/10)), 3, 0, -90);
+  upArrow.graphics.beginFill("#eee").drawPolyStar((tempWidth*(5/11)), (tempHeight*(5/13)), (tempWidth*(1/6)), 3, 0, -90);
 
   //increasing the temperature
   upArrow.addEventListener("click", function (evt) {
@@ -50,7 +48,7 @@ function createTemp() {
   });
 
   var downArrow = new createjs.Shape();
-  downArrow.graphics.beginFill("#eee").drawPolyStar((tempWidth*(4/5)), (tempHeight*(2/3)), (tempWidth*(1/10)), 3, 0, 90);
+  downArrow.graphics.beginFill("#eee").drawPolyStar((tempWidth*(5/11)), (tempHeight*(10/12)), (tempWidth*(1/6)), 3, 0, 90);
 
   downArrow.addEventListener("click", function (evt) {
     degrees = degrees - .1;
@@ -62,18 +60,18 @@ function createTemp() {
   //creating and styling temperature text
   var temperature = new createjs.Text();
       temperature.text = (degrees);
-      temperature.color = "#666";
+      temperature.color = "#fff";
       temperature.font = "15px arial";
-      temperature.x = (screenWidth / 5);
-      temperature.y = (screenHeight / 4);
+      temperature.x = (screenWidth / 8);
+      temperature.y = (screenHeight * (5/9));
 
   //creating and styling F in farenheit
   var farenheit = new createjs.Text();
       farenheit.text = "F"
-      farenheit.color = "#666";
+      farenheit.color = "#fff";
       farenheit.font = "bold 10px arial";
-      farenheit.x = (screenWidth * (10/13));
-      farenheit.y = (screenHeight / 5);
+      farenheit.x = (screenWidth * (9/14));
+      farenheit.y = (screenHeight *(5/9));
 
   makeTemp();
 
@@ -83,7 +81,6 @@ function createTemp() {
         tempContainer.addChild(screenContainer);
         tempContainer.addChild(upArrow);
         tempContainer.addChild(downArrow);
-        screenContainer.addChild(tempScreen);
         screenContainer.addChild(temperature);
         screenContainer.addChild(farenheit);
         stage.update();
